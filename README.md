2bfa5183100da889192498cc216691b25c45d96a
Hitesh Lad <hiteshklad@gmail.com>
2015-03-18 19:49:14 -0700

    task 2.3 adding basic ng-route and ng-model

---
+++ README.md
@@ -0,0 +1,1 @@
+Clone of Task Rabbit using angularjs and firebase.
--- scripts/app.js
+++ scripts/app.js
@@ -7,11 +7,21 @@ var app = angular
     'ngRoute',    
     'firebase'
   ])
-  .constant('FURL', 'https://your-firebase.firebaseio.com/')  
+  .constant('FURL', 'https://josa.firebaseio.com/')  
   .config(function ($routeProvider) {
     $routeProvider      
       .when('/', {
         templateUrl: 'views/main.html'        
+      })
+      .when('/edit', {
+        templateUrl: 'views/edit.html'
+      })
+      .when('/post', {
+        templateUrl: 'views/post.html',
+        controller: 'TaskController'
+      })
+      .when('/browse', {
+        templateUrl: 'views/browse.html'
       })
       .otherwise({
         redirectTo: '/'
