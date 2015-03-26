59aaf788d11cecb9736ed353cc15b676207de2ec
Hitesh Lad <hiteshklad@gmail.com>
2015-03-25 20:56:06 -0700

    Task 2 : Authentication - 4 Adding notification with Toaster

--- scripts/controllers/nav.js
+++ scripts/controllers/nav.js
@@ -1,12 +1,13 @@
 'use strict';
 
-app.controller('NavController', function($scope, $location, Auth) {
+app.controller('NavController', function($scope, $location, Auth, toaster) {
 	
 	$scope.currentUser = Auth.user;
 	$scope.signedIn = Auth.signedIn;
 	
 	$scope.logout = function() {
-		Auth.logout();
+		Auth.logout();	
+      toaster.pop('success', 'Logged out successfully');
 		console.log("Logged out!");
 		$location.path('/');	
 	};
